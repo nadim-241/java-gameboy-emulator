9cf8093f37f8ab6bf3153dc15ecba575344fcb44
Nadim Searight <ns241@st-andrews.ac.uk>
2024-04-10 14:00:19 +0100

    Initial commit

---
+++ src/Register.java
@@ -0,0 +1,101 @@
+public class Register {
+    private short a;
+    private short b;
+    private short c;
+    private short d;
+    private short e;
+    private short f;
+    private short h;
+    private short l;
+
+    public void setA(short value) {
+        a = (short)(value & 0xFF);
+    }
+    public short getA() {
+        return(short)(a & 0xFF);
+    }
+    public void setB(short value) {
+        b = (short)(value & 0xFF);
+    }
+    public short getB() {
+        return(short)(b & 0xFF);
+    }
+    public void setC(short value) {
+        c = (short)(value & 0xFF);
+    }
+    public short getC() {
+        return(short)(c & 0xFF);
+    }
+    public void setD(short value) {
+        d = (short)(value & 0xFF);
+    }
+    public short getD() {
+        return(short)(d & 0xFF);
+    }
+    public void setE(short value) {
+        e = (short)(value & 0xFF);
+    }
+    public short getE() {
+        return(short)(e & 0xFF);
+    }
+    public void setF(short value) {
+        f = (short)(value & 0xFF);
+    }
+    public short getF() {
+        return(short)(f & 0xFF);
+    }
+    public void setH(short value) {
+        h = (short)(value & 0xFF);
+    }
+    public short getH() {
+        return(short)(h & 0xFF);
+    }
+    public void setL(short value) {
+        l = (short)(value & 0xFF);
+    }
+    public short getL() {
+        return(short)(l & 0xFF);
+    }
+
+
+    public int getBc() {
+        return ((b & 0xFF) << 8 | c & 0xFF) & 0xFFFF;
+    }
+
+    public void setBc(short value) {
+        value = (short)(value & 0xFFFF);
+        setB((short)((value & 0xFF00) >> 8));
+        setC((short)(value & 0x00FF));
+    }
+
+    public int getAf() {
+        return ((a & 0xFF) << 8 | f & 0xFF) & 0xFFFF;
+    }
+
+    public void setAf(short value) {
+        value = (short)(value & 0xFFFF);
+        setA((short)((value & 0xFF00) >> 8));
+        setF((short)(value & 0x00FF));
+    }
+
+    public int getDe() {
+        return ((d & 0xFF) << 8 | e & 0xFF) & 0xFFFF;
+    }
+
+    public void setDe(short value) {
+        value = (short)(value & 0xFFFF);
+        setD((short)((value & 0xFF00) >> 8));
+        setE((short)(value & 0x00FF));
+    }
+
+    public int getHl() {
+        return ((h & 0xFF) << 8 | l & 0xFF) & 0xFFFF;
+    }
+
+    public void setHl(short value) {
+        value = (short) (value & 0xFFFF);
+        setH((short) ((value & 0xFF00) >> 8));
+        setL((short) (value & 0x00FF));
+    }
+
+}
---
+++ tests/RegisterTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterTest {
+    private Register register;
+    @BeforeEach
+    void setUp() {
+        register = new Register();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBc() {
+        register.setB((short)0xf);
+        register.setC((short)0xc);
+        Assertions.assertEquals(register.getBc(), 0x0f0c);
+
+        register.setB((short)0xff);
+        register.setC((short)0xff);
+        Assertions.assertEquals(register.getBc(), 0xffff);
+    }
+
+    @Test
+    void setBc() {
+        register.setBc((short)0xFF0C);
+        Assertions.assertEquals(register.getBc(), 0xFF0C);
+    }
+
+    @Test
+    void getAf() {
+        register.setA((short)0xf);
+        register.setF((short)0xc);
+        Assertions.assertEquals(register.getAf(), 0x0f0c);
+
+        register.setA((short)0xff);
+        register.setC((short)0xff);
+        Assertions.assertEquals(register.getAf(), 0xffff);
+    }
+
+    @Test
+    void setAf() {
+        register.setAf((short)0xFF0C);
+        Assertions.assertEquals(register.getAf(), 0xFF0C);
+    }
+
+    @Test
+    void getDe() {
+        register.setD((short)0xf);
+        register.setE((short)0xc);
+        Assertions.assertEquals(register.getDe(), 0x0f0c);
+
+        register.setD((short)0xff);
+        register.setE((short)0xff);
+        Assertions.assertEquals(register.getDe(), 0xffff);
+    }
+
+    @Test
+    void setDe() {
+        register.setDe((short)0xFF0C);
+        Assertions.assertEquals(register.getDe(), 0xFF0C);
+    }
+
+    @Test
+    void getHl() {
+        register.setHl((short)0xFF0C);
+        Assertions.assertEquals(register.getHl(), 0xFF0C);
+    }
+}
